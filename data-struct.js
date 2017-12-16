baeb89a3a09073ce6f63ed59cc584535e94afe23
kmokidd <kmokidd@gmail.com>
2017-12-16 21:26:17 +0800

    :rocket: modify data structure

--- data-struct.js
+++ data-struct.js
@@ -1,58 +1,36 @@
 {
   keyword1: {
-    seg1: [{
-      record1: [rank, delta, hotness, count, color]
-    }, {
-      record2: [rank, delta, hotness, count, color]
-    }, {
-      record3: [rank, delta, hotness, count, color]
-    }, {
-      record4: [rank, delta, hotness, count, color]
-    }],
-    seg2: [{
-      record1: [rank, delta, hotness, count, color]
-    }, {
-      record2: [rank, delta, hotness, count, color]
-    }, {
-      record3: [rank, delta, hotness, count, color]
-    }, {
-      record4: [rank, delta, hotness, count, color]
-    }, {
-      record5: [rank, delta, hotness, count, color]
-    }, {
-      record6: [rank, delta, hotness, count, color]
-    }, {
-      record7: [rank, delta, hotness, count, color]
-    }, {
-      record8: [rank, delta, hotness, count, color]
-    }]
-  }
+    record1: [rank, delta, hotness, count],
+    record2: [rank, delta, hotness, count],
+    record3: [rank, delta, hotness, count],
+    record4: [rank, delta, hotness, count],
+    color: 'green'
+  },
   keyword2: {
-    seg1: [{
-      record1: [rank, delta, hotness, count, color]
-    }, {
-      record2: [rank, delta, hotness, count, color]
-    }, {
-      record3: [rank, delta, hotness, count, color]
-    }, {
-      record4: [rank, delta, hotness, count, color]
-    }],
-    seg2: [{
-      record1: [rank, delta, hotness, count, color]
-    }, {
-      record2: [rank, delta, hotness, count, color]
-    }, {
-      record3: [rank, delta, hotness, count, color]
-    }, {
-      record4: [rank, delta, hotness, count, color]
-    }, {
-      record5: [rank, delta, hotness, count, color]
-    }, {
-      record6: [rank, delta, hotness, count, color]
-    }, {
-      record7: [rank, delta, hotness, count, color]
-    }, {
-      record8: [rank, delta, hotness, count, color]
-    }]
+    record1: [rank, delta, hotness, count],
+    record2: [rank, delta, hotness, count],
+    record4: [rank, delta, hotness, count],
+    record5: [rank, delta, hotness, count],
+    record6: [rank, delta, hotness, count],
+    record7: [rank, delta, hotness, count],
+    record8: [rank, delta, hotness, count],
+    color: 'red'
+  }
+  keyword3: {
+    record1: [rank, delta, hotness, count],
+    record2: [rank, delta, hotness, count],
+    record3: [rank, delta, hotness, count],
+    record4: [rank, delta, hotness, count],
+    color: 'black'
+  },
+  keyword4: {
+    record1: [rank, delta, hotness, count],
+    record2: [rank, delta, hotness, count],
+    record4: [rank, delta, hotness, count],
+    record5: [rank, delta, hotness, count],
+    record6: [rank, delta, hotness, count],
+    record7: [rank, delta, hotness, count],
+    record8: [rank, delta, hotness, count],
+    color: 'red'
   }
 }
